520cc9b03a6d84ed7a250ab1847a3c18ee6754fd
abykm <abykm164@gmail.com>
2023-05-25 14:37:15 +0530

    1 file modified

--- src/components/user/add.component.js
+++ src/components/user/add.component.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input, Button, Row, Col } from 'reactstrap';
 import Menubar from '../header/menubar.component';
 
 export default function Adduser() {
   return (
     <p>
       <Menubar />
-      Hello I am add user
+      <div className="App">
+        <Row>
+          <Col sm={{ size: 6, order: 2, offset: 1 }}>
+            <h2>Add Users</h2>
+            <Form className="form">
+              <FormGroup>
+                <Label for="exampleEmail">Username : </Label>
+                <Input
+                  type="text"
+                  name="uname"
+                  id="exampleEmail"
+                  placeholder="user name"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Place : </Label>
+                <Input
+                  type="text"
+                  name="place"
+                  id="examplePassword"
+                  placeholder="place"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormGroup>
+              <Button color="success" href="/123">
+                Add
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </p>
   );
 }
