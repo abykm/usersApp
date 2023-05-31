5bf5102a84a263038ca06b6e4bbbe022e20e4d8f
abykm <abykm164@gmail.com>
2023-05-31 22:45:40 +0530

    1 file modified

--- src/components/user/add.component.js
+++ src/components/user/add.component.js
@@ -1,43 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createUsers } from '../../actions/users';
 import { Form, FormGroup, Label, Input, Button, Row, Col } from 'reactstrap';
 import Menubar from '../header/menubar.component';
 
 export default function Adduser() {
+  const [users, setUsers] = useState({ uname: '', place: '' });
+  const [submitted, setSubmitted] = useState(false);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUsers({ ...users, [name]: value });
+  };
+  const saveUsers = () => {
+    console.log(users);
+    setSubmitted(true);
+  };
+  const newUsers = () => {
+    setSubmitted(false);
+  };
   return (
     <div>
       <Menubar />
-      <div className="App">
-        <Row>
-          <Col sm={{ size: 6, order: 2, offset: 1 }}>
-            <h2>Add Users</h2>
-            <Form className="form">
-              <FormGroup>
-                <Label for="exampleEmail">Username : </Label>
-                <Input
-                  type="text"
-                  name="uname"
-                  id="exampleEmail"
-                  placeholder="user name"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="examplePassword">Place : </Label>
-                <Input
-                  type="text"
-                  name="place"
-                  id="examplePassword"
-                  placeholder="place"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </FormGroup>
-              <Button color="success" href="/123">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </div>
+      {submitted ? (
+        <div>
+          <p>Submitted Successfully</p>
+          <Button color="primary" onClick={newUsers}>
+            {' '}
+            New User{' '}
+          </Button>
+        </div>
+      ) : (
+        <div className="App">
+          <Row>
+            <Col sm={{ size: 6, order: 2, offset: 1 }}>
+              <h2>Add Users</h2>
+              <Form className="form">
+                <FormGroup>
+                  <Label for="exampleEmail">Username : </Label>
+                  <Input
+                    type="text"
+                    name="uname"
+                    id="exampleEmail"
+                    placeholder="user name"
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Place : </Label>
+                  <Input
+                    type="text"
+                    name="place"
+                    id="examplePassword"
+                    placeholder="place"
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+                <Button color="success" onClick={saveUsers}>
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </div>
+      )}
     </div>
   );
 }
